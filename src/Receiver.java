38bf7308e1e75a37ba8efccb17d46da009e38fce
LongPeng <longpeng96@qq.com>
2023-05-10 16:40:55 +0800

    fix bug

--- src/Receiver.java
+++ src/Receiver.java
@@ -142,8 +142,10 @@ public class Receiver {
             receiverSocket.send(replyPacket);
 
             if (this.receiveFIN) {
-                System.out.println("receive FIN, closing...");
-                this.receiverSocket.close();
+                System.out.println("ACK of FIN has been sent, to avoid this ACK" +
+                        "get lost, receiver will wait for 3 seconds for the possible" +
+                        "FIN from sender, then receiver will close.");
+                this.receiverSocket.setSoTimeout(3000);
                 return;
             }
         }
